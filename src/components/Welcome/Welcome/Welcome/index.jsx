638ae5b4fa977b568c936a47889a887ba83470e8
Tatiana Arias <tati1305@utp.edu.co>
2024-07-11 15:53:09 -0500

    UPDATE Welecome component

---
+++ src/components/Welcome/Welcome/Welcome/index.jsx
@@ -0,0 +1,16 @@
+import './welcome.css'
+
+function Welcome() {
+    return (
+        <div>
+            <h1>Welcome to Codeable Keep</h1>
+            <form action="">
+                <label htmlFor="">username</label>
+                <input type="text" name="" id="" placeholder='some-user' />
+                <button>Enter</button>
+            </form>
+        </div>
+    )
+}
+
+export default Welcome;
