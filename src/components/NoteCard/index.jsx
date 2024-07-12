a64dce4bcb5994b7d64aacaf62bfbac5513ea027
Tatiana Arias <tati1305@utp.edu.co>
2024-07-11 22:20:47 -0500

    fix errors in deshboard

--- src/components/NoteCard/index.jsx
+++ src/components/NoteCard/index.jsx
@@ -1,6 +1,6 @@
 
-import {styles} from './styles.module.css'
+import styles from './styles.module.css'
 
-export function NoteCard(){
+export function NoteCard() {
 
 }
--- src/components/NoteCreate/index.jsx
+++ src/components/NoteCreate/index.jsx
@@ -1,5 +1,5 @@
-import {styles} from './styles.module.css'
+import styles from './styles.module.css'
 
 export function NoteCreate() {
-    
+
 }
